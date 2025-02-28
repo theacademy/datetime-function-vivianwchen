ff50bb33019175c08580b8627d6d521936122748
vivianc125 <vchenship@gmail.com>
2025-02-28 12:06:54 -0600

    Commit 6 - getTimeDifference()

--- dateTimeFunction/src/main/java/com/example/DateTimeFunctions.java
+++ dateTimeFunction/src/main/java/com/example/DateTimeFunctions.java
@@ -62,7 +62,14 @@ public class DateTimeFunctions {
 	 */
 	public static String getTimeDifference(LocalDate date1, LocalDate date2) {
 		//YOUR CODE STARTS HERE
-		return null;
+
+		Period diff = date1.between(date2);
+
+		String formatted = String.format("Year-%d:Months-%d:Days-%d",
+				diff.getYears,
+				diff.getMonths(),
+				diff.getDays());
+		return formatted;
 		//YOUR CODE ENDS HERE
 
 	}
